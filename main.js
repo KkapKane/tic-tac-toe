b16e9ade6af9171be841b9ba874a83fa2bf56b83
Kane <LisaxKane@gmail.com>
2022-10-27 17:50:47 -0500

    added winner display and logic

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let Turn = 0;
 let GameBoard = {
-    
+    Display: document.querySelector('.displayWinner'),
     board: [' ', ' ', ' ',
             ' ', ' ', ' ',
             ' ', ' ', ' '],
@@ -22,10 +22,12 @@ function placeMarker(spot) {
                     GameBoard.board[spot] = this.player;
                     (this.gridSpot.textContent = this.player), GameBoard.showBoard();
                     Turn++;
+                    Winner();
                 } else if (Turn % 2 !== 0) {
                     GameBoard.board[spot] = this.bot;
                     (this.gridSpot.textContent = this.bot), GameBoard.showBoard();
                     Turn++;
+                    Winner();
                 }
             }
             
@@ -34,5 +36,90 @@ function placeMarker(spot) {
     }
 }
 
+function Winner() {
+  if 
+     (GameBoard.board[0] == "X" &&
+      GameBoard.board[1] == "X" &&
+      GameBoard.board[2] == "X" ||
+      GameBoard.board[3] == "X" &&
+      GameBoard.board[4] == "X" &&
+      GameBoard.board[5] == "X" ||
+      GameBoard.board[6] == "X" &&
+      GameBoard.board[7] == "X" &&
+      GameBoard.board[8] == "X" ||
+      GameBoard.board[0] == "X" &&
+      GameBoard.board[3] == "X" &&
+      GameBoard.board[6] == "X" ||
+      GameBoard.board[1] == "X" &&
+      GameBoard.board[4] == "X" &&
+      GameBoard.board[7] == "X" ||
+      GameBoard.board[2] == "X" &&
+      GameBoard.board[5] == "X" &&
+      GameBoard.board[8] == "X" ||
+      GameBoard.board[6] == "X" &&
+      GameBoard.board[4] == "X" &&
+      GameBoard.board[2] == "X" ||
+      GameBoard.board[0] == "X" &&
+      GameBoard.board[4] == "X" &&
+      GameBoard.board[8] == "X")
+  {
+    GameBoard.Display.textContent = "Player Wins"
+ 
+    
+  }
+  else if 
+     (GameBoard.board[0] == "O" &&
+      GameBoard.board[1] == "O" &&
+      GameBoard.board[2] == "O" ||
+      GameBoard.board[3] == "O" &&
+      GameBoard.board[4] == "O" &&
+      GameBoard.board[5] == "O" ||
+      GameBoard.board[6] == "O" &&
+      GameBoard.board[7] == "O" &&
+      GameBoard.board[8] == "O" ||
+      GameBoard.board[0] == "O" &&
+      GameBoard.board[3] == "O" &&
+      GameBoard.board[6] == "O" ||
+      GameBoard.board[1] == "O" &&
+      GameBoard.board[4] == "O" &&
+      GameBoard.board[7] == "O" ||
+      GameBoard.board[2] == "O" &&
+      GameBoard.board[5] == "O" &&
+      GameBoard.board[8] == "O" ||
+      GameBoard.board[6] == "O" &&
+      GameBoard.board[4] == "O" &&
+      GameBoard.board[2] == "O" ||
+      GameBoard.board[0] == "O" &&
+      GameBoard.board[4] == "O" &&
+      GameBoard.board[8] == "O")
+     {
+      GameBoard.Display.textContent = "Bot wins!"
+    
+    
+  }
+ else if (
+      GameBoard.board[0] !== " " &&
+      GameBoard.board[1] !== " " &&
+      GameBoard.board[2] !== " " &&
+      GameBoard.board[3] !== " " &&
+      GameBoard.board[4] !== " " &&
+      GameBoard.board[5] !== " " &&
+      GameBoard.board[6] !== " " &&
+      GameBoard.board[7] !== " " &&
+      GameBoard.board[8] !== " "
+  ) {
+    GameBoard.Display.textContent = "Draw";
+     
+      
+    }
+    
+}
+const cleargrid = document.querySelectorAll('grid');
 
-//placeMarker(4).draw();
+function resetGame(element) {
+    GameBoard.board = [' ', ' ', ' ',
+        ' ', ' ', ' ',
+        ' ', ' ', ' '];
+    element.textContent = ' ';
+   
+}
